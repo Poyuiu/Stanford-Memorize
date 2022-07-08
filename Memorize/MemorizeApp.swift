83a57a1e8fd3a157db877fa5d5ada94a6d1b05dd
poyu <yoyohihirobert@gmail.com>
2022-07-09 02:25:36 +0800

    first commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by poyu on 2022/7/9.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
